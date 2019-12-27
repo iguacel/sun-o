7b66608887dcf6919240ce158e6b9184bbdda2b5
J.A. Iguacel <infoiguacel@gmail.com>
2019-12-27 22:56:31 +0100

    Updates

---
+++ precache-manifest.a04f6cbbc195cd644ab5061b85ee4d51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b5ef6ba08d40cbac0e05fb947d2963",
+    "url": "/sun-o/index.html"
+  },
+  {
+    "revision": "d5203b37fee7fc976f6a",
+    "url": "/sun-o/static/css/main.f41808ea.chunk.css"
+  },
+  {
+    "revision": "2d30d34f156d917957e3",
+    "url": "/sun-o/static/js/2.f486da1b.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/sun-o/static/js/2.f486da1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5203b37fee7fc976f6a",
+    "url": "/sun-o/static/js/main.234420d9.chunk.js"
+  },
+  {
+    "revision": "a1e5f916b48717105c8c",
+    "url": "/sun-o/static/js/runtime-main.f95237ed.js"
+  }
+]);
