1adba50a84c8e379aa10541ec49840469b972a9c
J.A. Iguacel <infoiguacel@gmail.com>
2020-03-02 15:23:47 +0100

    dep

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer(props) {
           rel="noopener noreferrer"
           title="MeeusSunMoon Github"
         >
-          <strong> MeeusSunMoon</strong> (Gina Häußge),{" "}
+          <strong> MeeusSunMoon</strong> (Jan Greis),{" "}
         </a>
         <a
           href="https://github.com/mbostock/solar-calculator"
